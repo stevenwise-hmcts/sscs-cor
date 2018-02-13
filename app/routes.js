fa9e0f4ff922bc4ffe4244a176918be468e8fd60
Steve Wise <steven.wise@hmcts.net>
2018-02-13 11:40:24 +0000

    Add branching for evidence options

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,22 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+router.get('/evidence-options', function (req, res) {
+	res.render('evidence-options');
+})
+
+router.post('/evidence-options', function (req, res) {
+
+	if (req.body['radio-group'] === 'Post it') {
+		res.redirect('/evidence-post')
+	} else if (req.body['radio-group'] === 'Upload it') {
+		res.redirect('/evidence-upload')
+	} else {
+		res.redirect('/evidence-statement')
+	}
+
+})
+
 // add your routes here
 
 module.exports = router
